4ca8319779e3c48300c8ecee701168d9648bd8ac
Inn-J <inn.javanicha@gmail.com>
2025-09-22 14:13:50 +0700

    add test1and utils

---
+++ dist/Utils.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.helloWorld = helloWorld;
+exports.add = add;
+function helloWorld() {
+    return "Hello, World";
+}
+function add(a, b) {
+    return a + b;
+}
